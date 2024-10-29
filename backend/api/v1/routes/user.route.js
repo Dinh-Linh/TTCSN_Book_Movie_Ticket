869e729687a0062d36a1030767b2c2e8cfed829b
huydev <huy040424@gmail.com>
2024-10-30 00:18:12 +0700

    Gắn frontend vào views trong back end

--- backend/api/v1/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const UserController = require("../controllers/User.controller");
-const express = require("express");
-const Router = express.Router();
-
-Router.post("/login", UserController.login)
-Router.post("/register", UserController.register)
-
-
-module.exports = Router
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const bodyParser = require('body-parser')
-const routeAPI = require('./api/v1/routes/index.route')
+const path = require('path')
+const flash = require('express-flash')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const routeAPI = require('./routes/api/index.route')
+const routeViews = require("./routes/views/index.route")
 
 require("dotenv").config()
 
@@ -11,13 +16,32 @@ const app = express()
 const database = require("./config/database/index")
 database.connectDB();
 
+// views engine
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+// data public
+app.use(express.static(path.join(__dirname, 'public')))
+
 // parse application/json
 app.use(bodyParser.json())
+app.use(express.urlencoded({
+    extended: true,
+}))
+
+app.use(cookieParser('keyboard cat'));
+app.use(session({ cookie: { maxAge: 60000 } }));
+
+// flash messages
+app.use(flash());
+
+
 
 // routes
+routeViews(app)
 routeAPI(app)
 
-// listening PORT 3001
+// listening PORT 3000
 app.listen(process.env.PORT, () => {
     console.log(`app listening ${process.env.PORT}`)
 })
